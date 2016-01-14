7b1b942e6d25d04cef144fd9f0da1b13d5ab4981
Brian Stovall <hoppenmaus@gmail.com>
2016-01-13 21:48:34 -0500

    refactoring - everything is a mess right now

--- main.lua
+++ main.lua
@@ -1,58 +1,78 @@
 function love.load()
-	key = 'no key pressed yet.'
-	avatar = love.graphics.newImage('image/avatar.png')
-	anims = {}
-	anims['up'] = {}
-	anims['left'] = {}
-	anims['down'] = {}
-	anims['right'] = {}
-	for i = 0, 8 do
-		anims['up'][i+1] = love.graphics.newQuad(i*64, 0, 64, 64, avatar:getDimensions())
-		anims['left'][i+1] = love.graphics.newQuad(i*64, 64, 64, 64, avatar:getDimensions())
-		anims['down'][i+1] = love.graphics.newQuad(i*64, 128, 64, 64, avatar:getDimensions())
-		anims['right'][i+1] = love.graphics.newQuad(i*64, 192, 64, 64, avatar:getDimensions())
+	spriteList =  []
+	local avatar = Sprite('image/avatar.png') 
+	avatar = makeAnims(avatar, ['up', 'left', 'down', 'right'], 9, 6.5, 64, 64)
+	spriteList['avatar'] = avatar
+	spriteList.avatar.dress = function () {
+		--animation frame selecting code
+		if sprite.stopped == true then
+			sprite.frame = 1
+		else
+			sprite.fullSecs = fullSecs + dt * framerate
+			if fullSecs >= 1 then
+				frame = frame + math.floor(fullSecs)
+				fullSecs = fullSecs - math.floor(fullSecs)
+			end
+			if frame > 9 then frame = (frame % 9) + 1 end
+		end
+	}
+end
+
+
+function Sprite( imageDir, x, y, dx, dy) 
+	local spriteT = {}
+	spriteT['img'] = love.graphics.newImage(imageDir)
+	spriteT.x = x or 0
+	spriteT.y = y or 0
+	spriteT.dx = dx or 0
+	spriteT.dy = dy or 0
+	sprite.animated = false
+  return spriteT
+end
+
+--costume names should be entered from top to bottom of spritesheet
+function makeAnims (sprite, costumes, framesPerRow, frameRate, cellHeight, cellWidth)
+	sprite.animated = true
+	sprite.stopped = true
+	sprite.fullSecs = 0
+	sprite.frame = 1
+	local dim = sprite.img:getDimensions()
+	for v, k in pairs(costumes) do
+		for i = 0, framesPerRow - 1 do
+			sprite.costumes[k][i+1] = love.graphics.newQuad(i*cellWidth, (v-1) * cellHeight, cellWidth, cellHeight, dim)
+		end
 	end
-	frame = 1
-	framerate = 6.5
-	fullSecs = 0
-	walkSpeed = 140 -- walking speed in px/sec
-	avatarX = 250
-	avatarY = 250
-	stopped = true
-	direction = 'down'
+	sprite.frameRate = frameRate
+	return sprite
 end
 
 function love.update(dt)
-	--animation frame selecting code
-	if stopped == true then
-		frame = 1
-	else
-		fullSecs = fullSecs + dt * framerate
-		if fullSecs >= 1 then
-			frame = frame + math.floor(fullSecs)
-			fullSecs = fullSecs - math.floor(fullSecs)
-		end
-		if frame > 9 then frame = (frame % 9) + 1 end
-	end
-	-- movement code
-	if stopped == false then
-		if direction == 'right' then
-			avatarX = avatarX + walkSpeed * dt
-		elseif direction == 'left' then
-			avatarX = avatarX - walkSpeed * dt
-		elseif direction == 'up' then
-			avatarY = avatarY - walkSpeed * dt
-		elseif direction == 'down' then
-			avatarY = avatarY + walkSpeed * dt
+	for sprite in spriteList do
+		-- movement code
+		if stopped == false then
+			if direction == 'right' then
+				avatarX = avatarX + walkSpeed * dt
+			elseif direction == 'left' then
+				avatarX = avatarX - walkSpeed * dt
+			elseif direction == 'up' then
+				avatarY = avatarY - walkSpeed * dt
+			elseif direction == 'down' then
+				avatarY = avatarY + walkSpeed * dt
+			end
 		end
 	end
-
 end
 
 function love.keypressed( keyPress, scancode, isrepreat )
 	if keyPress == 'escape' then love.event.quit() end
-	stopped = false
-	direction = keyPress
+	local isLegal = false
+	for k in pairs(anims) do
+		if keyPress == k then isLegal = true end
+	end
+	if isLegal == true then
+		stopped = false
+		direction = keyPress
+	end
 end
 
 function love.keyreleased( keyPress )
